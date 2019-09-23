485b8c19a23170a7afd1cbe075631f11c8395c37
sage-jordan <sagemjrodan@gmail.com>
2019-09-22 22:01:45 -0400

    started todoList

---
+++ reducer-app/src/components/todoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { initialState } from '../reducers/todoReducer';
+
+export default function todoList () {
+    return (
+        <div className="todo-list">
+            {}
+        </div>
+    )
+}

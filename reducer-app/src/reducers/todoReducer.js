1aa336661a04f1761ee248085454a38209a34a93
sage-jordan <sagemjrodan@gmail.com>
2019-09-22 21:56:31 -0400

    added initialState export data

--- reducer-app/src/reducers/todoReducer.js
+++ reducer-app/src/reducers/todoReducer.js
@@ -1,3 +1,21 @@
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'Reducers project',
+        completed: false,
+        id: 4927483920
+    },
+    {
+        item: 'One on One Review',
+        completed: false,
+        id: 0275630234
+    }
+]
+
 export const todoReducer = (state, action) => {
     switch(action.type) {
         default:
